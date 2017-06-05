9ac2a52ddf661d045dfe84c1082bc93228d3ed62
Adam O'Hern <adam@mechanicalcolor.com>
2017-06-05 17:30:39 -0400

    remove cruft

--- lxserv/solo.py
+++ lxserv/solo.py
@@ -3,46 +3,37 @@
 import lx
 import lxifc
 import lxu
+import modo
 import lxu.command
 import traceback
+import solo
 
-class solo_toggle(lxu.command.BasicCommand):
+class solo_toggle(solo.CommanderClass):
 
-    def __init__(self):
-        lxu.command.BasicCommand.__init__(self)
+    def commander_arguments(self):
+        return [
+            {
+                'name': 'mode',
+                'datatype': 'string',
+                'flags': ['optional']
+            }, {
+                'name': 'isActive',
+                'datatype': 'boolean',
+                'flags': ['query', 'optional']
+            }
+        ]
 
-        # ARGUMENTS DEFINED HERE:
-        self.dyna_Add('mode', lx.symbol.sTYPE_STRING)
-        self.basic_SetFlags(0, lx.symbol.fCMDARG_OPTIONAL)
 
-        # First we need to make this argument type a BOOLEAN
-        self.dyna_Add('active', lx.symbol.sTYPE_BOOLEAN)
-        self.basic_SetFlags(1, lx.symbol.fCMDARG_OPTIONAL | lx.symbol.fCMDARG_QUERY)
+    def commander_execute(self, msg, flags):
 
-    def basic_Execute(self, msg, flags):
-        try:
-            self.CMD_EXE(msg, flags)
-        except Exception:
-            lx.out(traceback.format_exc())
-            lx.eval('layout.createOrClose EventLog "Event Log_layout" title:@macros.layouts@EventLog@ width:600 height:600 persistent:true open:true')
-
-    def CMD_EXE(self, msg, flags):
-
-        # GRAB ARGUMENTS HERE:
-        if self.dyna_String(0, 0.0):
-            active = self.dyna_String(0, 0.0)
-        else:
-            active = 0
-
-        # CODE GOES HERE:
         # See if the user value exists
-        if lx.eval("query scriptsysservice userValue.isDefined ? solo")==0:
+        if lx.eval("query scriptsysservice userValue.isDefined ? solo") == 0:
             lx.eval( 'user.defNew solo boolean' );
             lx.eval( 'user.def solo username value:0' );
 
-        solo = lx.eval('user.value solo ?')
+        solo_is_active = lx.eval('user.value solo ?')
 
-        if solo==0:
+        if solo_is_active == 0:
             try:
                 lx.eval('hide.unsel')
                 lx.eval('item.refSystem')
@@ -68,20 +59,17 @@ class solo_toggle(lxu.command.BasicCommand):
 		return int(lx.eval('user.value solo ?'))
 
 
-    def cmd_Query(self,index,va):
-
+    def commander_query(self,index):
         if index == 1:
             # The active argument is at index 1
-
-            solo = self.checkState()
-
-            # we now cast the unknown object to a Value array object
-            # and add an int value of 0 or 1
-            valObj = lx.object.ValueArray(va)
-            valObj.AddInt(solo)
-
+            return self.checkState()
 
     def basic_Enable(self,msg):
-        return True
+        if self.checkState():
+            return True
+        elif modo.Scene().selected:
+            return True
+        else:
+            return False
 
 lx.bless(solo_toggle, "solo.toggle")
